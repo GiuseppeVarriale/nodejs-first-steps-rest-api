e5e39204f7d90350180345a8ddf450265dbdc63a
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-05-22 02:04:09 +0000

    Add NeDB

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,27 @@
+let NeDB = require("nedb");
+let db = new NeDB({
+  filename: "/home/vagrant/restful/db/users.db",
+  autoload: true,
+});
+
 module.exports = (app) => {
   app.get("/users", (req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.json({
-      users: [
-        {
-          name: "teste",
-          email: "contato@teste.comw",
-          id: 1,
-        },
-      ],
-    });
+    db.find({})
+      .sort({ name: 1 })
+      .exec((err, users) => {
+        if (err) {
+          console.log(`error: ${err}`);
+          res.status(400).json({
+            error: err,
+          });
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json({
+            users,
+          });
+        }
+      });
   });
 
   app.get("/users/admin", (req, res) => {
@@ -22,8 +33,15 @@ module.exports = (app) => {
   });
 
   app.post("/users", (req, res) => {
-
-    res.json(req.body);
+    db.insert(req.body, (err, user) => {
+      if (err) {
+        console.log(`error: ${err}`);
+        res.status(400).json({
+          error: err,
+        });
+      } else {
+        res.status(201).json(user);
+      }
+    });
   });
-
 };
