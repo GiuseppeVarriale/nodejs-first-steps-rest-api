0eba14fbe37f43ccfdeb2272b0854cb30f36e289
Giuseppe Varriale <pepe.saoleo@gmail.com>
2020-05-28 02:57:18 +0000

    Change JSON and urlencoder size limit to 25mb

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const expressValidator = require("express-validator");
 
 let app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '25mb' }));
+app.use(bodyParser.json({limit: '25mb'}));
 app.use(expressValidator());
 
 consign().include("routes").include("utils").into(app);
